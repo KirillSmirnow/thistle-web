e01e338551848b3f770622b0112dbfdc89b3ead9
Kirill Smirnov <smirnowku@gmail.com>
2019-02-13 10:47:43 +0300

    q

--- src/client.js
+++ src/client.js
@@ -9,7 +9,7 @@ function vkAuth(vkAuth, callback) {
             console.log(response);
         },
         error: function (response) {
-            console.log(response);
+            console.log("Error: " + response.responseJSON.developerMessage);
         }
     });
 }
