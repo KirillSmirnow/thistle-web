38aa063be5d62d8c4e08c7258287c44f6c3b2021
Kirill Smirnov <smirnowku@gmail.com>
2019-02-28 23:47:30 +0300

    Styles

--- src/chats/chats.js
+++ src/chats/chats.js
@@ -1,3 +1,6 @@
+CHATS = [];
+SELECTED_CHAT = -1;
+
 // Initialize components
 
 // On page loaded
@@ -5,9 +8,8 @@
 if (localStorage.getItem("accessToken") == null) {
     signOut();
 } else {
-    switchToFriendsTab();
     updateProfile();
-    updateFriends();
+    updateChatsList();
 }
 
 // Controls
@@ -22,5 +24,59 @@ function signOut() {
 function updateProfile() {
     getProfile(function (profile) {
         $("#name").text(profile["firstName"] + " " + profile["lastName"]);
+    });
+}
+
+function updateChatsList() {
+    getChats(function (chats) {
+        CHATS = chats;
+        $("#chats").empty();
+        for (let i in CHATS) {
+            let element = $("<div>");
+            $("#chats").append(element);
+            element.addClass("chat-element");
+
+            let name = $("<span>");
+            element.append(name);
+            name.addClass("chat-name");
+            name.text(CHATS[i].name);
+
+            element.click(function () {
+                $("#chats").children("div").each(function (child) {
+                    $(this).removeClass("selected");
+                });
+                element.addClass("selected");
+                SELECTED_CHAT = i;
+                updateMessages();
+            });
+        }
+    });
+}
+
+function updateMessages() {
+    getMessages(CHATS[SELECTED_CHAT].id, function (messages) {
+        console.log(messages);
+
+        $("#messages").empty();
+        for (let i in messages) {
+            let element = $("<div>");
+            $("#messages").append(element);
+            element.addClass("chat-element");
+
+            let text = $("<span>");
+            element.append(text);
+            // text.addClass("chat-name");
+            text.text(messages[i].text);
+
+            let time = $("<span>");
+            element.append(time);
+            // time.addClass("chat-name");
+            time.text(messages[i].dateTime);
+
+            let author = $("<span>");
+            element.append(author);
+            // author.addClass("chat-name");
+            author.text(messages[i].author.firstName);
+        }
     });
 }
